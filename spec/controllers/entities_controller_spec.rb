7ad05150ad83a71b4abbb3448322589fd08aa153
marekp <marek.paralic@gmail.com>
2013-02-09 16:22:31 +0100

    first rspec for C with LocalID

---
+++ spec/controllers/entities_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe EntitiesController do
+  describe 'registering Entity by LocalID' do
+    it 'should call the model method that creates LocalID record' do
+      pending "we do not work with local id yet"
+    end
+    it 'should call the model method that creates Entity record that reference LocalID'
+    it 'should select the Entity Details template for rendering'
+    it 'should make the inserted data available to that template'
+  end
+end
